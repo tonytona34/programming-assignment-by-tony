461f42e3041c6a16beb88e0b34a4934be801da5d
tonytona34 <tonykiprop24@gmail.com>
2024-10-06 07:23:10 -0700

    Add files via upload

---
+++ loan.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main[]
+{
+int age;
+float salary;
+
+//prompt the user to enter age
+printf("enter the age:");
+scanf("%d",&age);
+
+//prompt the user to enter the amount of salary
+printf("enter the amount of salary:");
+scanf("%f",&salary);
+
+if(age >=21,salary>=21000){
+printf("congratulation you qualify for loan ");
+}
+else{
+printf("unfortunately we are unable to offer you a loan at this time");
+}
+return 0;
+}
+

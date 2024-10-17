36f95faf912cf6eb7bf0af4adc3a2fe1eab25f96
tonytona34 <tonykiprop24@gmail.com>
2024-10-17 13:24:20 -0700

    Add files via upload

---
+++ i d array program.c
@@ -0,0 +1,11 @@
+// 1d array c programme
+#include <stdio.h>
+int main() {
+    int i;
+    int mangoes [4]={8,5,3,7};
+    for(i=0,i<4;i++){
+        printf("mangoes %d=%d\n",i, mangoes [i]);
+    }
+	
+	return 0;
+}
